04eb4ae830a885de968d641b08df91e9e0e23434
Denis <pydenzk@gmail.com>
2022-09-11 23:10:27 +0300

    Добавлена форма регистрации. Crispy-form плохо стилизует.

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'crispy_forms',
+
     'shop',
+    'users',
+
 ]
 
 MIDDLEWARE = [
@@ -125,3 +130,5 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),  # ссылаемся на shop.urls, там джанго будет искать нужный адрес
+    path('users/', include('users.urls')),
 ]
 
 if settings.DEBUG:
